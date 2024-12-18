b2d72116886ccd2c4b7afb357804b15bd831311d
Siedemus <barsak1177@gmail.com>
2024-12-18 17:58:46 +0100

    add head tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: true },
+	app: {
+		head: {
+			title: "Lang Glider",
+			meta: [
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+				{ charset: "utf-8" },
+			],
+		},
+	},
+});
