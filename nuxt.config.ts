311707ab2ea73f7a8a51f95756c02a6b7b25b4f3
Siedemus <barsak1177@gmail.com>
2024-12-20 19:04:21 +0100

    add styles to config and add google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
+	css: ["public/css/main.css"],
+
 	app: {
 		head: {
 			title: "Lang Glider",
@@ -10,5 +12,16 @@ export default defineNuxtConfig({
 				{ charset: "utf-8" },
 			],
 		},
+	},
+
+	modules: ["@nuxtjs/google-fonts"],
+	googleFonts: {
+		families: {
+			"Funnel Display": [400, 500, 600, 700],
+		},
+		display: "swap",
+		preload: true,
+		download: true,
+		base64: false,
 	},
 });
