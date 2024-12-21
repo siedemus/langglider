bfd4d170822af721c7a881f5bcfc9d947ca2cc74
Siedemus <barsak1177@gmail.com>
2024-12-21 23:49:16 +0100

    add hero section template

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
 <template>
-    <div>test</div>
+    <section>
+        <h1>Accelerate Your Language Learning Journey</h1>
+        <p>Automate Your Flashcard Creation with AI - Spend More Time Learning</p>
+        <button>Get Started Now</button>
+    </section>
+    <section></section>
 </template>
+
+<style lang="css" scoped>
+
+</style>
