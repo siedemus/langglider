a7b31d2b5207ed9e02523d709f63773a912baa76
Siedemus <barsak1177@gmail.com>
2024-12-22 02:22:02 +0100

    modify navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,10 @@
                 <img class="header__logo" src="public/images/logo.png" />
                 <span class="header__span">LanGGlider</span>
             </NuxtLink>
-            <div>
-                <button class="header__button">Log In</button>
-                <button class="header__button header__button--secondary">Sign Up</button>
+            <div class="header__hamburger">
+                <div class="header__hamburger-line"></div>
+                <div class="header__hamburger-line"></div>
+                <div class="header__hamburger-line"></div>
             </div>
         </nav>
     </header>
@@ -16,13 +17,23 @@
 
 <style lang="css" scoped>
 .header {
-    width: 100%;
-    height: 60px;
+    max-width: 100vw;
+    max-height: 85px;
+    background-color: transparent;
+    padding: 15px 20px;
+
+    @media (max-width: 860px) {
+        padding: 10px;
+    }
+
+    @media (max-width: 460px) {
+        padding: 5px;
+    }
 }
 
 .header__navigation {
     width: 100%;
-    height: 100%;
+    max-height: 100%;
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -35,47 +46,36 @@
 }
 
 .header__logo {
-    width: 60px;
-    height: 60px;
+    width: 85px;
+    height: 85px;
+
+    @media (max-width: 460px) {
+        width: 75px;
+        height: 75px;
+    }
 }
 
 .header__span {
     color: #1B946A;
-    font-size: 35px;
-    font-weight: 600;
+    font-size: 45px;
+    font-weight: 700;
 
-    @media (max-width: 520px) {
+    @media (max-width: 560px) {
         display: none;
     }
 }
 
-.header__button {
-    padding: 4px 8px;
-    font-weight: 500;
-    font-size: 20px;
-    font-weight: 700;
-    color: #1B946A;
-    border: 3px solid #1B946A;
-    border-radius: 6px;
-    transition: all 0.3s ease;
-    background-color: white;
-    font-family: inherit;
-    margin-right: 6px;
-}
-
-.header__button:hover {
+.header__hamburger {
     cursor: pointer;
-    background-color: #1B946A;
-    color: white;
+    padding: 20px;
 }
 
-.header__button--secondary {
+.header__hamburger-line {
+    width: 40px;
+    height: 5px;
+    border-radius: 7px;
     background-color: #1B946A;
-    color: white;
-}
-
-.header__button--secondary:hover {
-    background-color: white;
-    color: #1B946A;
+    margin: 5px 0;
+    transition: 0.3s;
 }
 </style>
