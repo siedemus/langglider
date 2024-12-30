6e36478542c213889304407d8e62f9c3875bd9c4
Siedemus <barsak1177@gmail.com>
2024-12-30 22:52:02 +0100

    add supabase + google font + basic configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/supabase', "@nuxtjs/google-fonts"],
+
+  runtimeConfig: {
+    public: {
+      appUrl: ""
+    }
+  },
+
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_ANON_KEY,
+    redirectOptions: {
+      login: "/login",
+      callback: "/confirm",
+      exclude: ["/"],
+    },
+  },
+
+  googleFonts: {
+    families: {
+      "Funnel Display": {
+        wght: [400, 600, 700, 800],
+      }
+    }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>LangGlider</div>
+</template>
+
+<style lang="css" scoped>
+div {
+    font-family: "Funnel Display";
+}
+</style>
