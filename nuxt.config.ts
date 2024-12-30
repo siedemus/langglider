97ffe6fa3e0bb705034b440023058b24e3e3a74b
Siedemus <barsak1177@gmail.com>
2024-12-30 23:06:00 +0100

    add normalize and global styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/supabase', "@nuxtjs/google-fonts"],
+  app: {
+    head: {
+      link: [{ rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" }]
+    }
+  },
+  css: ["~/assets/css/main.css"],
 
   runtimeConfig: {
     public: {
