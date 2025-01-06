f5d06ea6c460cb5b8bd11b086b0c777a854b146a
Siedemus <barsak1177@gmail.com>
2025-01-06 23:50:38 +0100

    add new public route and create useAuth composable

---
+++ composables/useAuth.ts
@@ -0,0 +1,120 @@
+type AvailableProvider = "google" | "discord"
+type Error = null | string
+type Message = null | string
+
+export const useAuth = () => {
+    const { public: env } = useRuntimeConfig();
+    const { auth } = useSupabaseClient();
+    const user = useSupabaseUser()
+    const router = useRouter();
+
+    const loading = ref<boolean>(false);
+    const message = ref<Message>("");
+    const errorMessage = ref<Error>(null)
+    const isAuthenticated = user.value
+
+    const messageCleanUp = () => {
+        if (message.value && message.value.length > 0) message.value = null
+        if (errorMessage.value && errorMessage.value.length > 0) errorMessage.value = null
+    };
+
+    const signUp = async (email: string, password: string) => {
+        try {
+            loading.value = true;
+            messageCleanUp();
+
+            const { error } = await auth.signUp({ email, password });
+
+            if (error) throw error;
+
+            message.value = "Check your email for verification link";
+        } catch (error: any) {
+            errorMessage.value = error.message
+        } finally {
+            loading.value = false
+        }
+    };
+
+    const signInWithPassword = async (email: string, password: string) => {
+        try {
+            loading.value = true
+            messageCleanUp()
+
+            const { error } = await auth.signInWithPassword({ email, password });
+
+            if (error) throw error
+
+            router.push("/dashboard")
+        } catch (error: any) {
+            errorMessage.value = error.message
+        } finally {
+            loading.value = false
+        }
+    };
+
+    const signOut = async () => {
+        try {
+            loading.value = true
+            messageCleanUp()
+
+            const { error } = await auth.signOut()
+
+            if (error) throw error
+
+            router.push("/")
+        } catch (error: any) {
+            errorMessage.value = error.message
+        } finally {
+            loading.value = false
+        }
+    };
+
+    const signInWithOAuth = async (provider: AvailableProvider) => {
+        try {
+            loading.value = true
+            messageCleanUp()
+
+            const { error } = await auth.signInWithOAuth({
+                provider
+            })
+
+            if (error) throw error
+
+            router.push("/")
+        } catch (error: any) {
+            errorMessage.value = error.message
+        } finally {
+            loading.value = false
+        }
+    };
+
+    const requestPasswordChange = async (email: string) => {
+        try {
+            loading.value = true
+
+            const { error } = await auth.resetPasswordForEmail(email, {
+                redirectTo: `${env.appUrl}/reset`
+            })
+
+            if (error) throw error
+
+            message.value = "Check your email for password change link";
+        } catch (error: any) {
+            errorMessage.value = error.message
+        } finally {
+            loading.value = false
+        }
+    };
+
+    return {
+        loading,
+        message,
+        errorMessage,
+        isAuthenticated,
+        signUp,
+        signInWithPassword,
+        signInWithOAuth,
+        signOut,
+        requestPasswordChange
+    };
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
     redirectOptions: {
       login: "/login",
       callback: "/confirm",
-      exclude: ["/"],
+      exclude: ["/", "/reset"],
     },
   },
 
