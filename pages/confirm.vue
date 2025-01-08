58a94cc69d22a742dbe48f9ec0a98886e277a1d3
Siedemus <barsak1177@gmail.com>
2025-01-08 23:38:42 +0100

    cosmetic changes

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,19 +1,19 @@
+<template>
+    <div class="message-box">Wait a second...</div>
+</template>
+
 <script setup lang="ts">
 const user = useSupabaseUser()
 
 watch(user, () => {
     if (user.value) {
-        return navigateTo('/app')
+        return navigateTo('/')
     }
 }, { immediate: true })
 </script>
 
-<template>
-    <div class="messageBox">Wait a second...</div>
-</template>
-
 <style lang="css" scoped>
-.messageBox {
+.message-box {
     min-width: 100vw;
     min-height: 100vh;
     display: flex;
