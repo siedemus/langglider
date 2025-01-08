50e3eda1cc9c0b1c16c8f31c43eba16adb802a7d
Siedemus <barsak1177@gmail.com>
2025-01-08 20:16:55 +0100

    make tile animation smoother

--- components/ui/Tile/index.vue
+++ components/ui/Tile/index.vue
@@ -1,3 +1,12 @@
+<template>
+    <article class="tile" @mousemove="onMouseMove">
+        <h3 class="tile__header">{{ header }}</h3>
+        <span class="tile__text">
+            <slot />
+        </span>
+    </article>
+</template>
+
 <script setup lang="ts">
 interface TileProps {
     header: string
@@ -15,15 +24,6 @@ const onMouseMove = (e: MouseEvent) => {
     card.style.setProperty("--y", `${y}px`)
 }
 </script>
-
-<template>
-    <article class="tile" @mousemove="onMouseMove">
-        <h3 class="tile__header">{{ header }}</h3>
-        <span class="tile__text">
-            <slot />
-        </span>
-    </article>
-</template>
 
 <style lang="css" scoped>
 .tile {
@@ -47,9 +47,9 @@ const onMouseMove = (e: MouseEvent) => {
     top: var(--y);
     left: var(--x);
     transform: translate(-50%, -50%) scale(2);
-    background: radial-gradient(rgba(224, 224, 224, 0.2), transparent, transparent);
+    background: radial-gradient(rgba(224, 224, 224, 0.3), transparent, transparent);
+    transition: opacity 0.3s ease-out;
     opacity: 0;
-    transition: all 10ms;
 }
 
 .tile:hover::before {
@@ -58,14 +58,14 @@ const onMouseMove = (e: MouseEvent) => {
 
 .tile__header {
     color: white;
-    margin: 0 0 0.5rem;
+    margin: 0 0 0.2rem;
     font-weight: 800;
     font-size: 1.8rem;
     text-shadow: 0 0 10px rgba(255, 255, 255, 0.7);
 }
 
 .tile__text {
-    font-size: 1.3rem;
+    font-size: 1.2rem;
     font-weight: 400;
     text-shadow: 0 0 10px rgba(255, 255, 255, 0.7);
 }
