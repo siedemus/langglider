6e1cbc732c0cfff3ac1a253922adb586502b2119
Siedemus <barsak1177@gmail.com>
2025-01-21 20:51:45 +0100

    refactor request-reset page

--- pages/request-reset.vue
+++ pages/request-reset.vue
@@ -1,64 +1,44 @@
 <template>
-    <div class="gradient-background">
-        <section class="transparent-section">
-            <h1 class="transparent-section__header">
+    <main class="main">
+        <UiTransparentContainer>
+            <template #header>
                 Forgot Password?
-            </h1>
-            <form class="transparent-section__form" @submit.prevent="requestPasswordChange(email)">
-                <input :disabled="loading" type="email" class="form__input" placeholder="Email" v-model="email" />
-                <button class="form__button" type="submit">{{ loading ? "Processing..." : "Submit" }}</button>
-                <p v-if="message" class="form__message form__message--teal">{{ message }}</p>
-                <p v-if="errorMessage" class="form__message form__message--red">ERROR: {{ errorMessage }}</p>
-                <NuxtLink href="/" class="transparent-section__homeLink">Go to Home</NuxtLink>
-            </form>
-        </section>
-    </div>
+            </template>
+            <template #content>
+                <Form :loading="loading" :submit-func="submitForm" :inputs="inputs" />
+                <UiLink href="/">Go to Home</UiLink>
+                <UiMessageBox variant="Success" v-if="message">{{ message }}</UiMessageBox>
+                <UiMessageBox variant="Error" v-if="errorMessage">{{ errorMessage }}</UiMessageBox>
+            </template>
+        </UiTransparentContainer>
+    </main>
 </template>
 
 <script setup lang="ts">
+import type { FormProps } from '~/components/Form.vue';
+
 const email = ref("");
 
 const { loading, errorMessage, message, requestPasswordChange } = useAuth();
+
+const inputs: FormProps["inputs"] = [
+    {
+        attributes: { type: "email", placeholder: "Insert email" },
+        vModel: { value: email.value, updateValue: (value) => email.value = value }
+    }
+]
+
+const submitForm = async () => {
+    await requestPasswordChange(email.value)
+}
 </script>
 
 <style lang="css" scoped>
-@import url("~/assets/css/transparent-section.css");
-@import url("~/assets/css/green-link.css");
-
-@import url("~/assets/css/form/input.css");
-@import url("~/assets/css/form/button.css");
-@import url("~/assets/css/form/message.css");
-
-.gradient-background {
+.main {
     display: flex;
     justify-content: center;
     align-items: center;
-}
-
-.transparent-section {
-    max-width: 290px;
-    width: 100%;
-}
-
-.transparent-section__form {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-}
-
-.transparent-section__homeLink {
-    background-color: rgba(0, 255, 179, 0.7);
-    color: inherit;
-    padding: 0.8rem;
-    border: none;
-    border-radius: 0.5rem;
-    transition: all 0.3s;
-    cursor: pointer;
-    text-decoration: none;
-    text-align: center;
-
-    &:hover {
-        background-color: rgb(255, 255, 255, 0.2);
-    }
+    width: 100vw;
+    height: 100vh;
 }
 </style>
