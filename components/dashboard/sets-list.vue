4055dd390a0e9694a9c44a755e2086fba261f2e5
Siedemus <barsak1177@gmail.com>
2025-02-25 00:47:16 +0100

    fix links in sets-list and handle new loader

--- components/dashboard/sets-list.vue
+++ components/dashboard/sets-list.vue
@@ -1,18 +1,16 @@
 <template>
-    <div class="sets-list">
-        <div class="sets-list__container" v-if="loading">
-            <CommonLoader />
-        </div>
+    <SkeletonsSetsList v-if="loading" />
+    <div class="sets-list" v-else>
         <ul class="sets" v-if="!isEmpty">
-            <li class="sets__flashcards" v-for="set in data.sets">
-                <NuxtLink class="flashcards__link" :to="$route.path + `/${set.id}`">{{ set.name }}</NuxtLink>
+            <li class="sets__flashcard" v-for="set in data.sets" :key="set.id">
+                <NuxtLink class="flashcard__link" :to="$route.path + `/${set.id}`">{{ set.name }}</NuxtLink>
             </li>
         </ul>
         <div class="sets-list__container" v-else>
             <h2 class="container__header">There's no flashcards yet.</h2>
             <div class="container__buttons">
-                <CommonButtonLink to="/create">Create Flashcard Set</CommonButtonLink>
-                <CommonButtonLink to="/create-ai">Create Flashcard Set with AI</CommonButtonLink>
+                <CommonButtonLink to="/dashboard/create">Create Flashcard Set</CommonButtonLink>
+                <CommonButtonLink to="/dashboard/create-ai">Create Flashcard Set with AI</CommonButtonLink>
             </div>
         </div>
     </div>
@@ -23,7 +21,7 @@ interface SetsListProps { data: UserData; loading: boolean };
 
 const { data, loading } = defineProps<SetsListProps>();
 
-const isEmpty = data.sets.length === 0;
+const isEmpty = computed(() => data.sets.length === 0);
 </script>
 
 <style lang="css" scoped>
@@ -47,7 +45,7 @@ const isEmpty = data.sets.length === 0;
 
 .container__header {
     font-size: 2rem;
-    font-weight: 800;
+    font-weight: 600;
     margin: 0;
     padding: 1rem;
     text-align: center;
@@ -59,6 +57,7 @@ const isEmpty = data.sets.length === 0;
     align-items: center;
     gap: 0.5rem;
     flex-direction: column;
+    padding-bottom: 1rem;
 
     @media screen and (min-width: 620px) {
         flex-direction: row;
@@ -73,11 +72,11 @@ const isEmpty = data.sets.length === 0;
     gap: 1rem;
 }
 
-.sets__flashcards {
+.sets__flashcard {
     list-style: none;
 }
 
-.flashcards__link {
+.flashcard__link {
     text-decoration: none;
     color: inherit;
     display: block;
@@ -85,5 +84,12 @@ const isEmpty = data.sets.length === 0;
     background-color: var(--transparent-gray);
     padding: 1rem;
     border-radius: 7px;
+    transition: background-color 0.3s ease-in-out;
+    text-overflow: ellipsis;
+    overflow: hidden;
+}
+
+.flashcard__link:hover {
+    background-color: var(--transparent-white);
 }
 </style>
