64221b4983fb863678fa128b077e957b84465fc1
Siedemus <barsak1177@gmail.com>
2025-02-25 01:20:48 +0100

    add other way of handling errors in main layout

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,8 @@
 <template>
-    <NuxtLayout name="dashboard">
+    <CommonMessageBox class="message-box" variant="Error" v-if="userStore.error">
+        ERROR: {{ userStore.error }}
+    </CommonMessageBox>
+    <NuxtLayout name="dashboard" v-else>
         <NuxtPage />
     </NuxtLayout>
 </template>
@@ -7,8 +10,14 @@
 <script setup lang="ts">
 const userStore = useUserStore();
 
-await useAsyncData("setStoreInit", async () => {
+await useAsyncData("userStoreInit", async () => {
     await userStore.fetchUserData()
     return true
 });
 </script>
+
+<style lang="css" scoped>
+.message-box {
+    margin: 15px;
+}
+</style>
